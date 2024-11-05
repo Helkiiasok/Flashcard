45a68f2d28a52d07e1110b26f704497d60ba966e
Helkiiasok <168541783+Helkiiasok@users.noreply.github.com>
2024-11-05 08:51:21 -0300

    cards

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Biologia',
+    'Qual é a importância da biodiversidade para o equilíbrio dos ecossistemas?',
+    'Explorar a relação entre biodiversidade e ecossistemas pode revelar a complexidade da vida na Terra.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'A capital do Brasil é Brasília'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz Boa Noite em Inglês?',
+    'Boa Noite em ingles é Good night'
+)
